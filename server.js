b65e2370d9836820f338225a29960bc5d665cc23
Harvinder Chahal <harvinder.chahal@gmail.com>
2025-08-03 20:12:11 +0530

    Added API Welcome Route & Final Backend Structure for Deployment

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const apiRoutes = require('./api');
+const apiRoutes = require('./routes/api');
+app.use('/api', apiRoutes);
 
 dotenv.config();
 
